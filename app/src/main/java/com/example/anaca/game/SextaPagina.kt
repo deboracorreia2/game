3b910f2ca6ac266c113a8a8ae9e966c1501b47e0
Ana <anacaroline-2000@hotmail.com>
2019-03-16 06:09:31 -0300

    arrumei alertas e erros (total cinco paginas) -loading -conectar roupa -aguardar inicio batalha -tela de jogo -tela de pausa -tela de derrota

---
+++ app/src/main/java/com/example/anaca/game/SextaPagina.kt
@@ -0,0 +1,12 @@
+package com.example.anaca.game
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SextaPagina : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sexta_pagina)
+    }
+}
